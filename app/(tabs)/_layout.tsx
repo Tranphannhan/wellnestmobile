59cdb93f37ae7d4057048cfd59251213db378689
TranPhanNhan <nhannhaongo1997@gmail.com>
2025-08-06 18:26:22 +0700

    Lưu

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, View } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
@@ -20,24 +20,57 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Trang chủ',
+          tabBarIcon: ({ color }) => <IconSymbol size={24} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Tra cứu',
+          tabBarIcon: ({ color }) => <IconSymbol size={24} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="qr"
+        options={{
+          title: '',
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            <View
+              style={{
+                backgroundColor: Colors[colorScheme ?? 'light'].tint,
+                padding: 16,
+                borderRadius: 40,
+                top: -20,
+              }}
+            >
+              <IconSymbol size={32} name="qrcode.viewfinder" color="white" />
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="manual"
+        options={{
+          title: 'Thủ công',
+          tabBarIcon: ({ color }) => <IconSymbol size={24} name="pencil" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Tài khoản',
+          tabBarIcon: ({ color }) => <IconSymbol size={24} name="person.crop.circle" color={color} />,
         }}
       />
     </Tabs>
